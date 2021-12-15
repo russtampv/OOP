3e55f6546f23c7346030829816e841b68a2b8c2a
RodinRoman <roman.v.rodin@gmail.com>
2021-12-16 02:45:58 +0300

    lecture3

---
+++ lecture3_input_output_exceptions/src/test/java/FileAnalyserTests.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import ru.edu.lecture3.FileAnalyser;
+
+public class FileAnalyserTests {
+
+    private FileAnalyser fileAnalyser = null;
+
+    @Test
+    public void fileAnalyserTest() {
+    }
+}
